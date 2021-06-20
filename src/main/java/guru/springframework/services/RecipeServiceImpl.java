79b9f39f6e4aff0b4dc53425ef05bf1d24a8ffe6
Tim Johnson <johnsont53@gmail.com>
2021-06-20 12:07:27 -0400

    All unit tests and config.yml for circle ci

---
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -0,0 +1,33 @@
+package guru.springframework.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+	public final RecipeRepository recipeRepository;
+	
+	
+	
+	public RecipeServiceImpl(RecipeRepository recipeRepository) {
+		super();
+		this.recipeRepository = recipeRepository;
+	}
+
+	@Override
+	public Set<Recipe> getRecipes() {
+		log.debug("I'm in the service");
+		Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+        return recipeSet;
+	}
+
+}
