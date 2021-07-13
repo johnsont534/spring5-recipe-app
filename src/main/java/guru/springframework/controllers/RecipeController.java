e0fd368f0db9107e12eacc1d38735e68769187cc
Tim Johnson <johnsont53@gmail.com>
2021-07-13 11:34:18 -0400

    Last version before going to mysql

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 
 
@@ -24,6 +28,9 @@ import lombok.extern.slf4j.Slf4j;
 public class RecipeController {
 	
 	private final RecipeService recipeService;
+	
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
+
 
 	public RecipeController(RecipeService recipeService) {
 		super();
@@ -52,7 +59,18 @@ public class RecipeController {
 	}
 	
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+		
+		 if(bindingResult.hasErrors()){
+
+	            bindingResult.getAllErrors().forEach(objectError -> {
+	                log.debug(objectError.toString());
+	            });
+
+	            return RECIPE_RECIPEFORM_URL;
+	      }
+		
+		
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
 	    return "redirect:/recipe/" + savedCommand.getId() + "/show";
@@ -82,6 +100,7 @@ public class RecipeController {
 	    return modelAndView;
 	}
 	
+	/**
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(NumberFormatException.class)
 	public ModelAndView handleNumberFormatError(Exception exception) {
@@ -96,4 +115,5 @@ public class RecipeController {
 
 	    return modelAndView;
 	}
+	*/
 }
